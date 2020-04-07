7cdd332632acf3fd188b57df29defc8554fd11ce
toprakerzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-04-07 23:08:14 +0300

    Functions for FCM

--- Functions/src/index.ts
+++ Functions/src/index.ts
@@ -1,18 +1,80 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { Message } from './model';
 admin.initializeApp();
 const _db = admin.firestore();
 const userRef = _db.collection('users');
-export const updateMessageAsRead = functions.firestore.document('users/{userId}/conversations/{conversationId}/messages/{messageId}').onWrite(async (snapshot,context) => {
+export const updateMessageAsRead = functions.firestore.document('users/{userId}/conversations/{conversationId}/messages/{messageId}').onWrite(async (snapshot, context) => {
     userRef.doc(context.params.userId)
-    .collection('conversations').doc(context.params.conversationId)
-    .collection('messages').doc(context.params.messageId).update('isSended',true).then(() => {
-        console.log('Başarılı');
-    }).catch(e => {
-        console.log(e);
-        console.log('HATA');
-    });
+        .collection('conversations').doc(context.params.conversationId)
+        .collection('messages').doc(context.params.messageId).update('isSended', true).then(() => {
+            console.log('Başarılı');
+        }).catch(e => {
+            console.log(e);
+            console.log('HATA');
+        });
     console.log(snapshot);
     console.log(context);
     console.log(context.params);
 });
+
+export const inComingMessageNotification = functions.firestore
+    .document('users/{userId}/conversations/{conversationId}/messages/{messageId}')
+    .onCreate(async (snap, context) => {
+
+        const messageData = snap.data() as Message;
+        const userId: string = context.params.userId as string;
+
+        // console.log(messageData);
+        // Push İçin Mesaj bilgileri ile Push Datası Oluşturma
+        const tokenRef = _db.collection('users').doc(userId).collection('devices');
+        const getTokenPromises = await tokenRef.get();
+
+        // The array containing all the user's tokens.
+        const tokens: any[] = getTokenPromises.docs.map((doc: any) => doc.data().token);
+        console.log(tokens);
+        const keys: any[] = getTokenPromises.docs.map((doc: any) => doc.data().key);
+        console.log(keys);
+
+        // Check if there are any device tokens.
+        if (getTokenPromises.size === 0) {
+            console.log('There are no notification tokens to send to.');
+            return 'There are no notification tokens to send to.';
+        }
+
+        const payload = {
+            notification: {
+                title: 'Chat Application',
+                body: messageData !== undefined ? messageData.messageContent : 'Boş Mesaj',
+                icon: messageData.sender?.avatar?.downloadURL || 'https://randomuser.me/api/portraits/men/49.jpg'
+            }
+        };
+
+        // Send notifications to all tokens.
+        if (userId === messageData.sender.userId) {
+            console.log('Mesaj sahibine bildirim gitmez!');
+            return 'Owner!';
+        }
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        // For each message check if there was an error.
+        const tokensToRemove: any[] = [];
+        response.results.forEach((result, index) => {
+            const error = result.error;
+            if (error) {
+                console.error('Failure sending notification to', tokens[index], error);
+                // Cleanup the tokens who are not registered anymore.
+                if (error.code === 'messaging/invalid-registration-token' ||
+                    error.code === 'messaging/registration-token-not-registered') {
+                    tokensToRemove.push(tokenRef.doc(keys[index]).delete());
+                }
+            }
+        });
+        return Promise.all(tokensToRemove);
+    });
+
+    // _db.collection('users').doc(userId).get().then((u) => {
+    //     const user = u.data() as User;
+    //     return admin.messaging().sendToDevice(user.device,payload)
+    // }).catch(e => {
+    //     console.log('HATA');
+    // });
---
+++ functions/src/model.ts
@@ -0,0 +1,158 @@
+export class User {
+    userId: string;
+    firstname: string;
+    lastname: string;
+    email : string;
+    creationTime: number;
+    device : string;
+    settings : Settings;
+    isActive: boolean;
+    isDeleted: boolean;
+    deletedTime: number;
+    avatar : Avatar;
+    bio : string
+    constructor(userId: string, firstname: string, lastname: string,email : string, creationTime: number,device : string,settings : Settings, isActive: boolean, isDeleted: boolean, deletedTime: number,avatar : Avatar,bio : string) {
+        this.userId = userId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.creationTime = creationTime;
+        this.device = device;
+        this.settings = settings;
+        this.isActive = isActive;
+        this.isDeleted = isDeleted;
+        this.deletedTime = deletedTime;
+        this.avatar = avatar;
+        this.bio = bio;
+    }
+}
+
+export class Settings{
+    darkTheme : boolean;
+    notify : boolean;
+    isPrivate : boolean
+    constructor(darkTheme : boolean,notify : boolean,isPrivate : boolean){
+        this.darkTheme = darkTheme;
+        this.notify = notify;
+        this.isPrivate = isPrivate;
+    }
+}
+
+export class Avatar{
+    avatarName : string;
+    contentType : string;
+    size : number;
+    isActive : boolean;
+    isDeleted : boolean;
+    deletedTime : number;
+    downloadURL ?: string;
+    constructor(avatarName : string,contentType : string,size : number,isActive : boolean,isDeleted : boolean,deletedTime : number,downloadURL ?: string){
+        this.avatarName = avatarName
+        this.contentType = contentType;
+        this.size = size
+        this.isActive = isActive;
+        this.isDeleted = isDeleted
+        this.deletedTime = deletedTime;
+        this.downloadURL = downloadURL;
+    }
+}
+
+export class Conversation {
+    conversationId: string;
+    createdTime: number;
+    owner: User;
+    deletedTime: number;
+    isActive: boolean;
+    isDeleted: boolean;
+    title: string;
+    constructor(conversationId: string, createdTime: number, owner: User, deletedTime: number, isActive: boolean, isDeleted: boolean, title: string) {
+        this.conversationId = conversationId;
+        this.createdTime = createdTime;
+        this.owner = owner;
+        this.deletedTime = deletedTime;
+        this.isActive = isActive;
+        this.isDeleted = isDeleted;
+        this.title = title;
+    }
+}
+
+export class Participant {
+    participantId: string;
+    conversationId: string;
+    isOnline: boolean;
+    type: number;
+    creationTime: number;
+    user: User;
+    constructor(participantId: string, conversationId: string, isOnline: boolean, type: number, creationTime: number, user: User) {
+        this.participantId = participantId;
+        this.conversationId = conversationId;
+        this.isOnline = isOnline;
+        this.type = type;
+        this.creationTime = creationTime;
+        this.user = user;
+    }
+}
+
+export class Message {
+    messageId: string;
+    messageType: number;
+    messageContent: string;
+    sender: User;
+    conversationId: string;
+    attachmentUrl: string;
+    attachmentThumbUrl: string;
+    createdTime: number;
+    deletedTime: number;
+    isActive: boolean;
+    isDeleted: boolean;
+    isRead: boolean;
+    isSended: boolean;
+    constructor(messageId: string, messageType: number, messageContent: string, sender: User, conversationId: string, attachmentUrl: string, attachmentThumbUrl: string, createdTime: number, deletedTime: number, isActive: boolean, isDeleted: boolean, isRead: boolean, isSended: boolean) {
+        this.messageId = messageId;
+        this.messageType = messageType;
+        this.messageContent = messageContent;
+        this.sender = sender;
+        this.conversationId = conversationId;
+        this.attachmentUrl = attachmentUrl;
+        this.attachmentThumbUrl = attachmentThumbUrl;
+        this.createdTime = createdTime;
+        this.deletedTime = deletedTime;
+        this.isActive = isActive;
+        this.isDeleted = isDeleted;
+        this.isRead = isRead;
+        this.isSended = isSended;
+    }
+}
+
+export class ConversationModel {
+    conversation: Conversation;
+    participants: Participant[];
+    messages: Message[]
+    constructor(conversation: Conversation, participants: Participant[], messages: Message[]) {
+        this.conversation = conversation;
+        this.participants = participants;
+        this.messages = messages;
+    }
+}
+
+export class UserModel {
+    user: User;
+    conversations: ConversationModel[];
+    follower: User[];
+    following: User[];
+    constructor(user : User, conversations : ConversationModel[],follower : User[],following : User[]) {
+        this.user = user;
+        this.conversations = conversations;
+        this.follower = follower;
+        this.following = following;
+    }
+}
+
+export class Update {
+    key : string;
+    value : any;
+    constructor(key : string, value : any) {
+      this.key = key;
+      this.value = value;
+    }
+  }
