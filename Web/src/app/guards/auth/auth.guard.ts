e633e79be8384deab707fce180c15a98c9b18a3d
toprakerzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-04-10 20:08:40 +0300

    clean authguard

--- Web/src/app/guards/auth/auth.guard.ts
+++ Web/src/app/guards/auth/auth.guard.ts
@@ -14,9 +14,7 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const currentUserId = this._auth.getCurrentUserId();
-    console.log(currentUserId);
     if (currentUserId && currentUserId !== 'null') {
-      console.log(true);
       return true;
     }
     this.router.navigate(['home/login'], { queryParams: { returnUrl: state.url } });
